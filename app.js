4216717fdf35b97c06f9956791a8e7e86264c30b
Nataliia Bokhonska <119nelyaterry@gmail.com>
2022-02-05 12:22:45 +0200

    add_db

--- app.js
+++ app.js
@@ -1,19 +1,21 @@
 import express from 'express';
 import logger from 'morgan';
 import cors from 'cors';
+import dotenv from 'dotenv';
 
+dotenv.config();
 
 // import usersRouter from './routes/api/users';
 
-const app = express()
+const app = express();
 
-const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short'
+const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short';
 
-app.use(logger(formatsLogger))
+app.use(logger(formatsLogger));
 
-app.use(cors())
+app.use(cors());
 
-app.use(express.json())
+app.use(express.json());
 
 // app.use('/api/users', usersRouter)
 
--- bin/server.js
+++ bin/server.js
@@ -1,7 +1,12 @@
 import app from '../app';
+import mongoose from 'mongoose';
 
-const PORT = process.env.PORT || 3000
+const { DB_HOST, PORT = 3000 } = process.env;
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+mongoose
+  .connect(DB_HOST)
+  .then(() => console.log('Database connect'), app.listen(PORT))
+  .catch((error) => {
+    console.log(error.message);
+    process.exit(1);
+  });
