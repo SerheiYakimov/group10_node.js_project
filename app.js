59e34e231946734f205865a31a261483d7676934
Nataliia Bokhonska <119nelyaterry@gmail.com>
2022-02-05 13:11:25 +0200

    error-middlewares

--- app.js
+++ app.js
@@ -19,12 +19,12 @@ app.use(express.json());
 
 // app.use('/api/users', usersRouter)
 
-// app.use((req, res) => {
-//   res.status(404).json({ message: 'Not found' })
-// })
+app.use((req, res) => {
+  res.status(404).json({ message: 'Not found' });
+});
 
-// app.use((err, req, res, next) => {
-//   res.status(500).json({ message: err.message })
-// })
+app.use((err, req, res, next) => {
+  res.status(err.status || 500).json({ message: err.message });
+});
 
 export default app;
