9cf094c6a2d6127eeac1b9a4795da86531a7a4a7
SerheiYakimov <ysergey2901@gmail.com>
2022-02-05 02:05:41 +0200

    test

--- bin/server.js
+++ bin/server.js
@@ -1,4 +1,6 @@
-const app = require('./app')
+import app from '../app';
+
+const PORT = process.env.PORT || 3000
 
 app.listen(3000, () => {
   console.log("Server running. Use our API on port: 3000")
