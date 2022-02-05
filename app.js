bb07351f7c6276c66bbf48dee288a0a556e1f220
SerheiYakimov <ysergey2901@gmail.com>
2022-02-05 02:12:55 +0200

    add readMe

--- app.js
+++ app.js
@@ -10,7 +10,9 @@ const app = express()
 const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short'
 
 app.use(logger(formatsLogger))
+
 app.use(cors())
+
 app.use(express.json())
 
 // app.use('/api/users', usersRouter)
