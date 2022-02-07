45433835f84c5ebcae2a68fdb39ba1dc4e1c245f
Anton Kovalchuck <antonkovalchuck@gmail.com>
2022-02-07 22:21:18 +0200

    upd

--- controllers/auth/index.js
+++ controllers/auth/index.js
@@ -1,6 +1,6 @@
-import registration from "./registration"
-import login from "./login"
-import logout from "./logout"
+import { registration } from "./registration"
+import { login } from "./login"
+import { logout } from "./logout"
 
 export default {
   registration,
