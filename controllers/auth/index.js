c3e7b586b97334dd8710de9226870d154bdc14b1
Nataliia Bokhonska <119nelyaterry@gmail.com>
2022-02-07 23:32:51 +0200

    terry-update-contr_auth

--- controllers/auth/index.js
+++ controllers/auth/index.js
@@ -1,9 +1,5 @@
-import { registration } from "./registration"
-import { login } from "./login"
-import { logout } from "./logout"
+import { registration } from './registration';
+import { login } from './login';
+import { logout } from './logout';
 
-export default {
-  registration,
-  login,
-  logout
-}
+export { registration, login, logout };
