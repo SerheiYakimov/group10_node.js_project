d31a2d35886a0ed8810ee1a8ad8c14001be0ccfd
SerheiYakimov <ysergey2901@gmail.com>
2022-02-12 12:48:33 +0200

    add path to homePage

--- controllers/users/verifyUser.js
+++ controllers/users/verifyUser.js
@@ -7,13 +7,7 @@ export const verifyUser = async (req, res, next) => {
   console.log(verifyToken);
   if (userFromToken) {
     await repositoryUsers.updateVerify(userFromToken.id, true);
-    return res
-    .status(HttpCode.OK)
-    .json({
-      status: 'success',
-      code: HttpCode.OK,
-      data: { message: 'Verification successful' },
-    })
+    return res.redirect(`${process.env.FRONTEND_URL}`)
   }
   return res
     .status(HttpCode.BAD_REQUEST)
