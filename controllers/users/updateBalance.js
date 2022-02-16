aa14fdb91b85e3d7f79bf2c21d159b6c9de5c697
Nataliia Bokhonska <119nelyaterry@gmail.com>
2022-02-17 00:13:20 +0200

    updateBalance

--- controllers/users/updateBalance.js
+++ controllers/users/updateBalance.js
@@ -1,15 +1,21 @@
 import pkg from 'http-errors';
 import authService from '../../services/auth';
+import { HttpCode } from '../../lib/constants';
 const { NotFound } = pkg;
 
-export const updateBalance = async (res, req, next) => {
+export const updateBalance = async (req, res) => {
   const { _id } = req.user;
   const { balance } = req.body;
+
   if (!balance) {
     throw new NotFound(`Transactions could not be found`);
   }
+
   await authService.setBalance(_id, balance);
-  const tempUserBalance = await authService.getBalance(userId);
+  console.log(await authService.setBalance(_id, balance));
+
+  const tempUserBalance = await authService.getBalance(_id);
+  console.log(tempUserBalance);
 
   return res.status(HttpCode.OK).json({
     status: 'success',
