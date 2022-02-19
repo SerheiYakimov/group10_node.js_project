e9476e1fc1714bd5a6e06ed37307085e6d3b240b
Nataliia Bokhonska <119nelyaterry@gmail.com>
2022-02-19 17:27:08 +0200

    Edited guard

--- middlewares/guard/guard.js
+++ middlewares/guard/guard.js
@@ -18,12 +18,20 @@ const guard = async (req, res, next) => {
   const token = req.get('authorization')?.split(' ')[1];
   const isValidToken = verifyToken(token);
   if (!isValidToken) {
-    throw new Unauthorized('Not authorized');
+    return res.status(HttpCode.UNAUTORIZED).json({
+      status: 'error',
+      code: HttpCode.UNAUTORIZED,
+      message: 'Not authorized',
+    });
   }
   const payload = jwt.decode(token);
   const user = await repositoryUsers.findById(payload.id);
   if (!user || user.token !== token) {
-    throw new Unauthorized('Not authorized');
+    return res.status(HttpCode.UNAUTORIZED).json({
+      status: 'error',
+      code: HttpCode.UNAUTORIZED,
+      message: 'Not authorized',
+    });
   }
   req.user = user;
   next();
